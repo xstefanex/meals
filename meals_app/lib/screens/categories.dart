2a3502a53c82a962f00c6b734526a7c5a7a13027
xstefanex <tekawww@gmail.com>
2024-11-27 20:33:42 -0300

    Add meals data and change app icon

--- meals_app/lib/screens/categories.dart
+++ meals_app/lib/screens/categories.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
