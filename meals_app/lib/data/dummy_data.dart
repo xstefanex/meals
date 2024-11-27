cd52ce56ecf374720b19af3da2809e033a3dc40e
xstefanex <tekawww@gmail.com>
2024-11-26 21:43:42 -0300

    Meals App create. Add initial features.

---
+++ meals_app/lib/data/dummy_data.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
+
+const availableCategories = [
+  Category(
+    id: 'c1', 
+    title: 'Italian',
+    color: Colors.red,
+  ),
+    Category(
+    id: 'c2', 
+    title: 'Japanese',
+    color: Colors.blue,
+  ),
+    Category(
+    id: 'c3', 
+    title: 'Brazilian',
+    color: Colors.green,
+  ),
+    Category(
+    id: 'c4', 
+    title: 'Fast Food',
+    color: Colors.yellow,
+  ),
+    Category(
+    id: 'c5', 
+    title: 'Asian',
+    color: Colors.orange,
+  ),
+    Category(
+    id: 'c6', 
+    title: 'Hamburguer',
+    color: Colors.brown,
+  ),
+  Category(
+    id: 'c7', 
+    title: 'Quick & Easy',
+    color: Color.fromARGB(221, 216, 9, 188),
+  ),
+    Category(
+    id: 'c8', 
+    title: 'Summer',
+    color: Colors.yellowAccent,
+  ),
+    Category(
+    id: 'c9', 
+    title: 'French',
+    color: Colors.purple,
+  ),
+    Category(
+    id: 'c10', 
+    title: 'Exotic',
+    color: Colors.lightGreenAccent,
+  ),
+    Category(
+    id: 'c11', 
+    title: 'Spanish',
+    color: Color.fromARGB(255, 0, 62, 168),
+  ),
+    Category(
+    id: 'c12', 
+    title: 'Fitness',
+    color: Colors.lightBlue,
+  ),
+];
